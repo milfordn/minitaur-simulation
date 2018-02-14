2eb045ff3bb3528da4f429fdec198ea8d4d14441
siekmanj <siekmanj@oregonstate.edu>
2018-02-13 19:12:51 -0800

    fixed typo, made makefile more concise

--- MuJoCoThing/PIDController.cpp
+++ MuJoCoThing/PIDController.cpp
@@ -2,7 +2,7 @@
 #include "include/mujoco.h"
 
 PIDController::PIDController(const char *f, char * actuatorNames[], int size) : ModelController(f) {
-	this->actuatorIDs = (int *)malloc(sizeof(int) * size); //allocate required amount of memory to store actuator IDs
+	this->actuatorIDs = new int[size]; //allocate required amount of memory to store actuator IDs
 	this->controller = new pid(100, 0.0, 10.0); //allocate memory for PID controller
 
 	//Use mj provided method for obtaining actuator IDs
@@ -20,14 +20,14 @@ PIDController::~PIDController() {
 void PIDController::step() {
 	double output;
 	if(data->qpos[0] < -2 && data->qvel[0] > 0){ //If the position of the root body is at height -2, and the velocity vectory isn't negative (not falling)
-		output = controller->calculateOutput(data->qpos[1], 0); //Calculate the PID output
+		output = controller->calculateOutput(data->qpos[1], 0); //Calculate the PID output from the motor angle
 	}else{
 		output = 0;
 	}
 	mj_step(model, data);
 	
 	//Set the actuators to the PID output
-	data->ctrl[0] = -output; 
-	data->ctrl[1gi] = output;
+	data->ctrl[0] = -output;
+	data->ctrl[1] = output;
 	
 }
