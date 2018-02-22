185471ec00273273069a6b61b2ebb29ca0ed9b1f
siekmanj <siekmanj@oregonstate.edu>
2018-02-21 19:29:48 -0800

    cleaned up code, made indexes take member variable IDs instead of hardcoded values

--- MuJoCoThing/PositionController.cpp
+++ MuJoCoThing/PositionController.cpp
@@ -2,25 +2,25 @@
 #include "include/mujoco.h"
 #include <iostream>
 #include <math.h>
+#include <random>
+
 using std::cout;
 using std::endl;
 
+//TODO - take four leg names as input, create subclass to handle individual leg position
+
 PositionController::PositionController(const char *f, char * actuatorNames[], int size, char *endeffectorName) : ModelController(f) {
-	this->angleController = new pid(2, 0.000, 10000.0); //allocate memory for PID controller
-	this->lengthController = new pid(2, 0.00, 10000.0);
-	this->m1 = new pid(5, 0.0, 10000);
-	this->m2 = new pid(5, 0.0, 10000);
-	this->actuatorIDs = new int[size]; //allocate required amount of memory to store actuator IDs
+	//allocate memory for PID controllers
+	this->angleController = new pid(15, 0.000, 5000.0); 
+	this->lengthController = new pid(15, 0.00, 5000.0);
+	this->m1 = new pid(25, 0.0, 50000);
+	this->m2 = new pid(25, 0.0, 50000);
+	this->actuatorIDs = new int[size];
 	
-	//Use mj provided method for obtaining actuator IDs
 	for (int i = 0; i < size; i++) { 
 		this->actuatorIDs[i] = mj_name2id(this->model, mjtObj::mjOBJ_ACTUATOR, actuatorNames[i]);
-		if(actuatorIDs[i] == -1){
-			cout << "ACTUATOR NOT FOUND ERROR" << endl;
-		}
 	}
 	this->endeffectorID = mj_name2id(this->model, mjtObj::mjOBJ_SITE, endeffectorName);
-	cout << endeffectorID << endl;
 	this->size = size;
 	this->tick = 0;
 }
@@ -30,66 +30,45 @@ PositionController::~PositionController() {
 }
 
 void PositionController::step() {
-	mj_kinematics(model, data);
+	mj_kinematics(model, data); //Calculate kinematics so that site_xpos isn't 0
 	
-	if(tick < 20000){
-		
-	}else if(tick < 40000){
-		setposX = -0.6;
-		setposY = -0.6;
-	}else if(tick < 60000){
-		setposX = 1;
-		setposY = 0;
-	}else if(tick < 80000){
-		setposX = -0.65;
-		setposY = -.2;
-	}else if(tick < 100000){
-		setposX = 0;
-		setposY = 1;
-	}
-	else if(tick < 120000){
-		setposX = .77;
-		setposY = .15;
-	}else{
-		setposX = 0;
-		setposY = -0.5;
+	//Every 20,000 ticks we pick a new random location that the leg has to orient itself to (physically impossible to be within 0.5 units of origin, so account for that special case)
+	if(tick % 20000 == 0){
+		double newx = ((double)rand()/(double)RAND_MAX) * 3 - 1.5;
+		double newy = ((double)rand()/(double)RAND_MAX) * 3 - 1.5;
+		while(!((newx > 0.5 || newx < -0.5) && (newy > 0.5 || newy < -0.5))){
+			newx = ((double)rand()/(double)RAND_MAX) * 3 - 1.5;
+			newy = ((double)rand()/(double)RAND_MAX) * 3 - 1.5;
+		}
+		setposX = newx;
+		setposY = newy;
 	}
 	
-	const double L1 = 0.5; //Length of thigh
-	const double L2 = 1; //Length of shin
-
-	
-	double x = data->site_xpos[0]; //x coord of end effector
-	double z = data->site_xpos[2] - 2; //z coord of end effector
+	double x = data->site_xpos[endeffectorID]; //x coord of end effector
+	double z = data->site_xpos[endeffectorID+2] - 2; //z coord of end effector
 	
 	double desiredTheta = atan2(setposY, setposX); //Since the motors are at x,y (0, 0)
 	double desiredLength = sqrt(setposY*setposY + setposX*setposX);
 	
-	double currentTheta = (data->qpos[0] + data->qpos[1])/2; 
-	double currentLength = sqrt(x*x + z*z);
+	double currentTheta = (data->qpos[0] + data->qpos[2])/2; //The current angle of the leg
+	double currentLength = sqrt(x*x + z*z); //The current distance from the motors to the end effector
 	
-	double m1pos = desiredTheta + acos((desiredLength + L1*L1 - L2*L2)/(2 * L1 * desiredLength));
-	double m2pos = desiredTheta - acos((desiredLength + L1*L1 - L2*L2)/(2 * L1 * desiredLength));
-	
-	double output_1;
-	double output_2;
+	double m1pos = desiredTheta + acos((desiredLength + L1*L1 - L2*L2)/(2 * L1 * desiredLength)); //desired position of motor1
+	double m2pos = desiredTheta - acos((desiredLength + L1*L1 - L2*L2)/(2 * L1 * desiredLength)); //desired position of motor2
 	
 	//METHOD 1
-	
-	output_1 = m1->calculateOutput(tick, m1pos, data->qpos[0]);
-	output_2 = m2->calculateOutput(tick, m2pos, data->qpos[2]);
-	data->ctrl[0] = output_1;
-	data->ctrl[1] = output_2;
+	double output_1 = m1->calculateOutput(tick, m1pos, data->qpos[0]);
+	double output_2 = m2->calculateOutput(tick, m2pos, data->qpos[2]);
+	data->ctrl[actuatorIDs[0]] = output_1;
+	data->ctrl[actuatorIDs[1]] = output_2;
 	
 	//METHOD 2
 	/*
-	output_1 = angleController->calculateOutput(tick, desiredTheta, currentTheta);
-	output_2 = lengthController->calculateOutput(tick, desiredLength, currentLength);
-	data->ctrl[0] = output_1 - output_2;
-	data->ctrl[1] = output_1 + output_2;
+	double output_1 = angleController->calculateOutput(tick, desiredTheta, currentTheta);
+	double output_2 = lengthController->calculateOutput(tick, desiredLength, currentLength);
+	data->ctrl[0] = output_1 + output_2;
+	data->ctrl[1] = output_1 - output_2;
 	*/
-	//cout << "DESIRED ANGLE: " << desiredTheta << " CURRENT ANGLE: " << currentTheta<< ", DESIRED LENGTH: " << desiredLength << ", CURRENT LENGTH: " << currentLength << endl; 
-	//cout << m1pos << ", " << m2pos << endl;
 	tick++;
 	
 }
--- MuJoCoThing/PositionController.h
+++ MuJoCoThing/PositionController.h
@@ -11,12 +11,13 @@ private:
 	int * actuatorIDs;
 	int endeffectorID;
 	int size;
-	pid *m1;
-	pid *m2;
-	pid *angleController;
-	pid *lengthController;
-	double setposY = 1;
-	double setposX = 0.5;
-	
+	pid *m1; //Torque-level PID controller for controlling angle of motor 1
+	pid *m2; //Torque-level PID controller for controlling angle of motor 2
+	pid *angleController; //PID Controller for controlling relative angle between motor 1 and 2
+	pid *lengthController; //PID Controller for controlling distance from origin to end effector
+	double setposY = 1; //Initial vertical setpoint
+	double setposX = 0.5; //Initial horizontal setpoint
+	const double L1 = 0.5; //Length of thigh
+	const double L2 = 1; //Length of shin
 };
 
