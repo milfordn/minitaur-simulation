4938a18207821e85e82b8de9ba5d36f404a9eb16
Andrey Kornilovich <andreykornilovich@gmail.com>
2018-03-07 10:21:59 -0800

    added ghost robotics sdk to the repo

---
+++ ghost-sdk/examples/test/example_behavior.cpp
@@ -0,0 +1,17 @@
+#include <SDK.h>
+#include <behavior.h>
+
+class example_behavior;
+
+int main(int arc, char* argv[]){
+	//create the minitaur lol
+	init(RobotParams_Type_MINITAUR, argc, argv);
+
+	example_behavior example;
+
+	//remove any built in behaviors, add our own
+	behaviors.clear();
+	behaviors.push_back(&example);
+	example begin();
+
+}
