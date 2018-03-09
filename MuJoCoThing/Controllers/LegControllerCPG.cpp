b5eb6f37070925fe27bad40ffd0812cf6f60abaa
milfordn <milfordn@oregonstate.edu>
2018-03-09 12:01:11 -0800

    preliminary work for implementing CPG Network

--- MuJoCoThing/Controllers/LegControllerCPG.cpp
+++ MuJoCoThing/Controllers/LegControllerCPG.cpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 
 LegControllerCPG::LegControllerCPG(char * f)
-	: ModelController(f), patternGenerator(1, 50, mjPI / 4), 
+	: ModelController(f),
 	ctrlR(-0.65, -0.05, -0.05), ctrlT(0.25, 0, 0.05) {
 	sensor1 = mj_name2id(this->model, mjtObj::mjOBJ_SENSOR, "thigh1_spos");
 	sensor2 = mj_name2id(this->model, mjtObj::mjOBJ_SENSOR, "thigh2_spos");
@@ -10,8 +10,19 @@ LegControllerCPG::LegControllerCPG(char * f)
 	motor2 = mj_name2id(this->model, mjtObj::mjOBJ_ACTUATOR, "thigh2_a");
 	sensorTouch = mj_name2id(this->model, mjtObj::mjOBJ_SENSOR, "foot_stouch");
 
-	patternGenerator.setPose(8, 4);
-	patternGenerator.step(0, 0.001);
+	patternGenerator = new CPGNode(1, 50, mjPI / 4);
+	patternGenerator->setPose(8, 4);
+	patternGenerator->step(0, 0.001);
+}
+
+LegControllerCPG::LegControllerCPG(mjModel * m, mjData * d, char * s1, char * a1, char * s2, char * a2, CPGNode * cpg) 
+	: ModelController(m, d), ctrlR(-0.65, -0.05, -0.05), ctrlT(0.25, 0, 0.05) {
+	sensor1 = mj_name2id(this->model, mjtObj::mjOBJ_SENSOR, s1);
+	sensor2 = mj_name2id(this->model, mjtObj::mjOBJ_SENSOR, s2);
+	motor1 = mj_name2id(this->model, mjtObj::mjOBJ_ACTUATOR, a1);
+	motor2 = mj_name2id(this->model, mjtObj::mjOBJ_ACTUATOR, a1);
+
+	patternGenerator = cpg;
 }
 
 void LegControllerCPG::step() {
@@ -23,12 +34,12 @@ void LegControllerCPG::step() {
 	double expectedR = 0.1 * sin(angleAvg) + 0.2 * sin(acos(cos(angleAvg) / 2));
 	double totalT = (angle1 + angle2);
 
-	patternGenerator.setFeedback(s - 0.1);
-	patternGenerator.step(data->time - timePrev);
+	patternGenerator->setFeedback(s - 0.1);
+	patternGenerator->step(data->time - timePrev);
 	timePrev = data->time;
 
-	double y = patternGenerator.getValueY();
-	double x = patternGenerator.getValueX();
+	double y = patternGenerator->getValueY();
+	double x = patternGenerator->getValueX();
 
 	double setT = x;
 	double setR = y > 0 ? 0.275 : 0.125;
--- MuJoCoThing/Controllers/LegControllerCPG.h
+++ MuJoCoThing/Controllers/LegControllerCPG.h
@@ -8,9 +8,10 @@
 class LegControllerCPG : public ModelController {
 public:
 	LegControllerCPG(char * f);
+	LegControllerCPG(mjModel * m, mjData * d, char * s1, char * a1, char * s2, char * a2, CPGNode * cpg);
 	void step() override;
 private:
-	CPGNode patternGenerator;
+	CPGNode * patternGenerator;
 	int sensor1, sensor2, sensorTouch, motor1, motor2;
 	mjtNum timePrev;
 	pid ctrlR, ctrlT;
