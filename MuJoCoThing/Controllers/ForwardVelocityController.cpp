de09d61a2e6c05dec3a6e4934824fa6b3e51a8be
siekmanj <siekmanj@oregonstate.edu>
2018-05-08 14:39:34 -0700

    updated controllers to work with new cpg, tweaked forwardvelocitycontroller

--- MuJoCoThing/Controllers/ForwardVelocityController.cpp
+++ MuJoCoThing/Controllers/ForwardVelocityController.cpp
@@ -1,15 +1,16 @@
 #include "ForwardVelocityController.h"
 #include "../include/mujoco.h"
 #include <iostream>
+#include <string>
 #include <math.h>
 #include <random>
-
 using std::cout;
 using std::endl;
 
 
 ForwardVelocityController::ForwardVelocityController(const char *f, const char * actuatorNames[], const char * jointNames[], const char * endeffectorNames[]) : ModelController(f) {
 	//allocate memory for leg position controllers
+
 	int m1 = mj_name2id(this->model, mjtObj::mjOBJ_ACTUATOR, actuatorNames[0]);
 	int m2 = mj_name2id(this->model, mjtObj::mjOBJ_ACTUATOR, actuatorNames[1]);
 	int m3 = mj_name2id(this->model, mjtObj::mjOBJ_ACTUATOR, actuatorNames[2]);
@@ -33,14 +34,39 @@ ForwardVelocityController::ForwardVelocityController(const char *f, const char *
 	int end3 = mj_name2id(this->model, mjtObj::mjOBJ_SITE, endeffectorNames[2]);
 	int end4 = mj_name2id(this->model, mjtObj::mjOBJ_SITE, endeffectorNames[3]);
 
+	cout << m1AngleID << ", " << m2AngleID << ", " << m3AngleID << ", " << m4AngleID << ", " << m5AngleID << ", " << m6AngleID << ", " << m7AngleID << ", " << m8AngleID << endl;
+	cout << end1 << ", " << end2 << ", " << end3 << ", " << end4 << endl;
+
 	this->frontLeft = new LegPositionController(m1, m2, m1AngleID, m2AngleID, end1);
 	this->frontRight = new LegPositionController(m3, m4, m3AngleID, m4AngleID, end2);
 	this->backLeft = new LegPositionController(m5, m6, m5AngleID, m6AngleID, end3);
 	this->backRight = new LegPositionController(m7, m8, m7AngleID, m8AngleID, end4);
+
+	double pP = 0.1;
+	double pI = 0.0;
+	double pD = 0.00001;
+	double max = 10;
+
+	double rP = 0.5;
+	double rI = 0.0;
+	double rD = 0.00001;
+
+	this->p = new pid(pP, pI, pD); //High-level PID controller for controlling pitch
+	this->r = new pid(rP, rI, rD); //High-level PID controller for controlling roll
+	p->limitOutput(-max, max);
+	r->limitOutput(-max, max);
+	speed = 3;
+
 	pair1Theta = 1.57;
-	pair2Theta = 1.57;;
-	pair1Speed = 1.0;
-	pair2Speed = -1.0;
+	pair2Theta = 1.57;
+	pair1Speed = speed;
+	pair2Speed = speed;
+
+	output.open("./telemetry.csv", std::ofstream::out | std::ofstream::trunc);
+	for(int i = 0; i < datapoints; i++){
+		output << "qpos[" << i << "],";
+	}
+	cout << "\n";
 	t = data->time;
 	tick = 0;
 }
@@ -50,71 +76,132 @@ ForwardVelocityController::~ForwardVelocityController() {
 }
 
 //theta = sin(sx - (1/2)sin(sx-P) - P)
-double ForwardVelocityController::calculateTheta(double speed, double t, double offset){
-	return PI/2 - (PI/6)*sin(speed*(t) - (0.5)*sin(speed*t - offset) - offset);
+double ForwardVelocityController::calculateTheta(double speed, double t, double offset, double bias){
+	return PI/2 - (PI/8)*sin(speed*(t) - (0.5)*sin(speed*t - offset) - offset);
 }
 
 //L = (cos(sx - P) - 1)*cos(-sin(sx - P) + sx - P) - h
 double ForwardVelocityController::calculateLength(double speed, double t, double offset, double height){
-	return 0.191 + 0.018 * (((cos(speed*t - offset)) - 1) * cos(-sin(speed*t - offset) + speed*t - offset) - height);
+	return 0.09*(((cos(speed*t - offset)) - 1) * cos(-sin(speed*t - offset) + speed*t - offset)) - height;
 }
 
 //T = -(2/5) * ((-cos(sx - P) - 1)*cos(-sin(sx - P) + sx - P) - (1/2))
 double ForwardVelocityController::calculateStiffness(double speed, double t, double offset){
-	return -(.4) * ((-(cos(speed*t - offset)) - 1) * cos(-sin(speed*t - offset) + speed*t - offset) - 0.5) * 0.3;
+	return -(.4) * ((-(cos(speed*t - offset)) - 1) * cos(-sin(speed*t - offset) + speed*t - offset) - 0.5) * 100;
 }
 void ForwardVelocityController::step() {
+
 	mj_kinematics(model, data); //Calculate kinematics
+
 	double t_delta = data->time - t;
 	t = data->time;
 
-  //FUNCTION TO MATCH: THETA = sin(x - 1/2sin(x))
-	double speed = 4;
+	double height1 = .5;
+	double height2 = .5;
+	double height3 = .5;
+	double height4 = .5;
 
-	double offset1 = 0;
-	double offset2 = PI/2;
-	double offset3 = PI;
-	double offset4 = 3*PI/2;
+	double pitch = data->qpos[0]; //pitch angle of body (needed to keep legs pointed at ground)
+	double stoop = data->qpos[1]; //height of body from ground
+	double roll = data->qpos[2]; //roll angle of body
 
-	double height1 = PI;
-	double height2 = PI;
-	double height3 = PI;
-	double height4 = PI;
+	double interval = PI;
+	double offset1 = 1 * interval;
+	double offset2 = 0 * interval;
+	double offset3 = 0 * interval;
+	double offset4 = 1 * interval;
 
+	double pitchControl = p->calculateOutput(t, 0.0, pitch);
+	double rollControl = r->calculateOutput(t, 0.0, roll);
 
-	double leg1Theta = calculateTheta(speed, t, offset1);
-	double leg2Theta = calculateTheta(speed, t, offset2);
-	double leg3Theta = calculateTheta(speed, t, offset3);
-	double leg4Theta = calculateTheta(speed, t, offset4);
+	//offset1 -= pitchControl + rollControl;
+	//offset2 += pitchControl - rollControl;
+	//offset3 += pitchControl + rollControl;
+	//offset4 -= pitchControl - rollControl;
+	//cout << pitchControl << " from " << pitch << ", and " << rollControl << " from " << roll << endl;
+	//cout << data->qpos[0] << ", " << data->qpos[1] << ", " << data->qpos[2] << ", " << data->qpos[3] << ", " << data->qpos[4] << endl;
 
-	double leg1Length = calculateLength(speed, t, offset1, height1);
-	double leg2Length = calculateLength(speed, t, offset2, height2);
-	double leg3Length = calculateLength(speed, t, offset3, height3);
-	double leg4Length = calculateLength(speed, t, offset4, height4);
+	//If backright is on the ground
 
-	double leg1Stiffness = calculateStiffness(speed, t, offset1);
-	double leg2Stiffness = calculateStiffness(speed, t, offset2);
-	double leg3Stiffness = calculateStiffness(speed, t, offset3);
-	double leg4Stiffness = calculateStiffness(speed, t, offset4);
+	double speed1 = pair1Speed; //Back right
+	double speed2 = pair2Speed; //Back left
+	double speed3 = pair1Speed; //Front right
+	double speed4 = pair2Speed; //Front left
 
+	double leg1Theta = calculateTheta(speed1, t, offset1, pitch);
+	double leg2Theta = calculateTheta(speed2, t, offset2, pitch);
+	double leg3Theta = calculateTheta(speed3, t, offset3, pitch);
+	double leg4Theta = calculateTheta(speed4, t, offset4, pitch);
 
-	frontLeft->setAngle(leg1Theta);
-	frontRight->setAngle(leg2Theta);
-	backLeft->setAngle(leg3Theta);
-	backRight->setAngle(leg4Theta);
+	double leg1Length = calculateLength(speed1, t, offset1, height1);
+	double leg2Length = calculateLength(speed2, t, offset2, height2);
+	double leg3Length = calculateLength(speed3, t, offset3, height3);
+	double leg4Length = calculateLength(speed4, t, offset4, height4);
 
-	frontLeft->setLength(leg1Length);
-	frontRight->setLength(leg2Length);
-	backLeft->setLength(leg3Length);
-	backRight->setLength(leg4Length);
+	double leg1Stiffness = calculateStiffness(speed1, t, offset1);
+	double leg2Stiffness = calculateStiffness(speed2, t, offset2);
+	double leg3Stiffness = calculateStiffness(speed3, t, offset3);
+	double leg4Stiffness = calculateStiffness(speed4, t, offset4);
 
-	frontLeft->setPgain(leg1Stiffness);
-	frontRight->setPgain(leg2Stiffness);
-	backLeft->setPgain(leg3Stiffness);
-	backRight->setPgain(leg4Stiffness);
+	//cout << "front left: " << speed1 << ", " << leg1Theta << ", back right: " << speed3 << ", " << leg3Theta << endl;
+	if(t < 0.2){
+		leg1Theta = PI/2;
+		leg2Theta = PI/2;
+		leg3Theta = PI/2;
+		leg4Theta = PI/2;
+		leg1Length = 0;
+		leg2Length = 0;
+		leg3Length = 0;
+		leg4Length = 0;
+		offset1 = 0;
+		offset2 = 0;
+		offset3 = 0;
+		offset4 = 0;
+	}
+	int max = 10000;
+	if(tick < max){
+		for(int i = 0; i < datapoints; i++){
+			output << data->qpos[i] << ",";
+		}
+		output << "\n";
+	}else if(tick == max){
+		output.close();
+		cout << "Telemetry file written.\n";
+	}
+	backRight->setAngle(leg1Theta);
+	backLeft->setAngle(leg2Theta);
+	frontRight->setAngle(leg3Theta);
+	frontLeft->setAngle(leg4Theta);
 
-	frontLeft->step(data, model);
-	frontRight->step(data, model);
-	backLeft->step(data, model);
+	backRight->setLength(leg1Length);
+	backLeft->setLength(leg2Length);
+	frontRight->setLength(leg3Length);
+	frontLeft->setLength(leg4Length);
+
+	backRight->setPgain(leg1Stiffness);
+	backLeft->setPgain(leg2Stiffness);
+	frontRight->setPgain(leg3Stiffness);
+	frontLeft->setPgain(leg4Stiffness);
+
 	backRight->step(data, model);
+	backLeft->step(data, model);
+	frontRight->step(data, model);
+	frontLeft->step(data, model);
+	if(t > 2) tick++;
+}
+void ForwardVelocityController::keyboardCallback(GLFWwindow*, int key, int, int act, int mod) {
+	/*cout << pair1Speed << endl;
+	if(act != GLFW_PRESS) return;
+	if(key == GLFW_KEY_W){
+		pair1Speed += 0.00125;
+		pair2Speed += 0.00125;
+	}
+	if(key == GLFW_KEY_S){
+		pair1Speed -= 0.00125;
+		pair2Speed -= 0.00125;
+	}
+	if(key == GLFW_KEY_A){
+	}
+	if(key == GLFW_KEY_D){
+	}*/
 }
--- MuJoCoThing/Controllers/LegPositionController.cpp
+++ MuJoCoThing/Controllers/LegPositionController.cpp
@@ -7,20 +7,19 @@
 using std::cout;
 using std::endl;
 
-//TODO - take four leg names as input, create subclass to handle individual leg position
-
 LegPositionController::LegPositionController(int m1ID, int m2ID, int m1JointID, int m2JointID, int endID){
 	//allocate memory for PID controllers
 	//this->angleController = new pid(15, 0.000, 5000.0);
 	//this->lengthController = new pid(15, 0.00, 5000.0);
-	this->m1 = new pid(0.3, 0.0, 10);
-	this->m2 = new pid(0.3, 0.0, 10);
+	this->m1 = new pid(100, 0.0, 1);
+	this->m2 = new pid(100, 0.0, 1);
 
 	this->m1ID = m1ID;
 	this->m2ID = m2ID;
 	this->m1JointID = m1JointID;
 	this->m2JointID = m2JointID;
 	this->endID = endID;
+	this->currentLength = 0.191;
 
 	tick = 0;
 }
@@ -30,22 +29,22 @@ LegPositionController::~LegPositionController() {
 }
 
 void LegPositionController::step(struct _mjData* data, struct _mjModel* model) {
-	const double L1 = 0.1;
-	const double L2 = 0.2;
+	L1 = 0.1;
+	L2 = 0.2;
 
 	double currentm1pos = data->qpos[model->jnt_qposadr[m1JointID]];
 	double currentm2pos = data->qpos[model->jnt_qposadr[m2JointID]];
 
-	double currentTheta = (currentm1pos + currentm2pos)/2; //The current angle of the leg
-	double currentLength = L1 * sin(currentTheta - currentm1pos) + sqrt(L2*L2 - L1*L1*cos(currentTheta - currentm1pos)*cos(currentTheta - currentm1pos)); //The current distance from the motors to the end effector
+	currentAngle = (currentm1pos + currentm2pos)/2; //The current angle of the leg
+	currentLength = L1 * sin(currentAngle - currentm1pos) + sqrt(L2*L2 - L1*L1*cos(currentAngle - currentm1pos)*cos(currentAngle - currentm1pos)); //The current distance from the motors to the end effector
 
 
 	double m1pos = desiredAngle - asin((desiredLength*desiredLength + L1*L1 - L2*L2)/(2 * L1 * desiredLength)); //desired pos of motor1
 	double m2pos = desiredAngle + asin((desiredLength*desiredLength + L1*L1 - L2*L2)/(2 * L1 * desiredLength)); //desired pos of motor2
 	//METHOD 1
-	double output_1 = m1->calculateOutput(tick, m1pos, currentm1pos);
-	double output_2 = m2->calculateOutput(tick, m2pos, currentm2pos);
-
+	double output_1 = m1->calculateOutput(data->time, m1pos, currentm1pos);
+	double output_2 = m2->calculateOutput(data->time, m2pos, currentm2pos);
+	//cout << "currentm1: " << currentm1pos << ", desired: " << m1pos << "output: " << output_1 << endl;
 	if(active){
 		data->ctrl[m1ID] = output_1;
 		data->ctrl[m2ID] = output_2;
@@ -53,16 +52,6 @@ void LegPositionController::step(struct _mjData* data, struct _mjModel* model) {
 		data->ctrl[m1ID] = 0;
 		data->ctrl[m2ID] = 0;
 	}
-
-	//METHOD 2
-	/*
-	double output_1 = angleController->calculateOutput(tick, desiredTheta, currentTheta);
-	double output_2 = lengthController->calculateOutput(tick, desiredLength, currentLength);
-	data->ctrl[0] = output_1 + output_2;
-	data->ctrl[1] = output_1 - output_2;
-	*/
-	tick++;
-
 }
 void LegPositionController::setPgain(double p){
 	this->m1->setPgain(p);
@@ -73,9 +62,19 @@ void LegPositionController::release(){
 }
 void LegPositionController::setLength(double length){
 	active = true;
-	desiredLength = length;
+	if(length > 1) length = 1;
+	if(length < -1) length = -1;
+	desiredLength = 0.191 + 0.09*length;
 }
 void LegPositionController::setAngle(double angle){
 	active = true;
 	desiredAngle = angle;
+}
+double LegPositionController::getLength(){
+	double input_range = 2*L1;
+	double output_range = 2.0;
+	return (currentLength - L1)*output_range / input_range + -1;
+}
+double LegPositionController::getAngle(){
+	return currentAngle;
 }
