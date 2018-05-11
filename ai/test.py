6945a2e0dd0ccbe09167b51e121069d221293496
Jonah Siekmann <siekmanj@oregonstate.edu>
2018-05-11 14:01:36 -0700

    minitaur model runs in python

--- ai/test.py
+++ ai/test.py
@@ -1,15 +1,17 @@
 import mujoco_py
 import gym
 import time
-import testenv.humanoid
+import minitaur.minitaur
 
-env = gym.make('testenv-v0')
+env = gym.make('Minitaur-v0')
 for i in range(200):
     observation = env.reset()
+    #space = gym.space.Discrete()
     for t in range(1000):
         env.render()
         #print(observation)
         action = env.action_space.sample()
+        #get action from CPGController
         observation, reward, done, _ = env.step(action)
         if done:
             #print("Episode finished.")
